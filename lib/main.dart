a42050fe8dd5f94ae4db4bc2a9034f2b6ed4b45a
Dmitrii Rodionov <dmitry@anna.money>
2025-08-20 17:35:41 +0200

    1 step start

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,5 @@
+/// TODO: Create MaterialApp.
+/// TODO: Create a screen with Scaffold + AppBar.
+/// TODO: Add a Text widget and a Button.
+/// TODO: Button changes state via setState.
 void main() {}
