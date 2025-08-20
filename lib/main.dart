eabd88ddbf459acaf4dc4c82d98220d483ca3283
Dmitrii Rodionov <dmitry@anna.money>
2025-08-20 17:45:45 +0200

    1 step finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,57 @@
-/// TODO: Create MaterialApp.
-/// TODO: Create a screen with Scaffold + AppBar.
-/// TODO: Add a Text widget and a Button.
-/// TODO: Button changes state via setState.
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+  String buttonText = 'Click me!';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 16,
+          children: [
+            Text('Hello, Flutter!'),
+            ElevatedButton(
+              onPressed: () => onClick(),
+              child: Text(buttonText),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onClick() {
+    setState(() {
+      counter++;
+      buttonText = 'Clicked! ($counter)';
+    });
+  }
+}
